df065707b455d5c0713a32fee4830bb713a06ec7
Wilson <wilsonchandu99@gmail.com>
2026-06-23 11:24:43 +0530

    updated backend URL

--- server.js
+++ server.js
@@ -974,14 +974,7 @@ app.get("/", (req, res) => {
     START SERVER
 ========================================================= */
 const PORT = process.env.PORT || 3000;
-async function sendWhatsApp(phone, message) {
-  console.log("📤 WhatsApp Function Called");
-  console.log("TO:", phone);
-  console.log("MSG:", message);
 
-  // 🔥 For now just simulate
-  return true;
-}
 app.listen(PORT, "0.0.0.0", () => {
   console.log(` Server running on port ${PORT}`);
 });
