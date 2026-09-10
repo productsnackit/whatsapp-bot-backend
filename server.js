6e523d1e470eeb18475bdc782967e9f6efd18fd2
Wilson Chandu <wilson@local>
2026-09-10 17:44:15 +0530

    Fix internal chat attachments

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const io = new Server(httpServer, {
 });
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "25mb" }));
 
 io.on("connection", (socket) => {
   socket.on("join-internal-room", ({ department }) => {
